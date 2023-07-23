bfa888f32a9ec2c6a1a7c361be0a24c24508b564
shaunemslie <shaunpemslie@gmail.com>
2023-07-23 18:44:22 +0200

    Commit2

--- Kata.Services/ParserService/Models/InputModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Kata.Services
-{
-    public class InputModel
-    {
-        public string DelimiterLine { get; set; }
-        public string RemainingInput { get; set; }
-    }
-}
---
+++ Kata.Services/ParserService/ReaderService.cs
@@ -0,0 +1,49 @@
+﻿namespace Kata.Services;
+
+public class ReaderService : IReaderService
+{
+    public IEnumerable<int> GetParsedNumbersFromInput(string input)
+    {
+        var reader = new StringReader(input);
+        var delimiters = GetDefaultAndExtractedDelimiters(reader.ReadLine());
+        var numbers = GetExtractedAndParsedNumbers(reader.ReadToEnd(), delimiters);
+
+        return numbers;
+    }
+
+    private IEnumerable<int> GetExtractedAndParsedNumbers(string input, IEnumerable<string> delimiters)
+    {
+        var numbers = input.Split(
+            delimiters.ToArray(),
+            StringSplitOptions.RemoveEmptyEntries
+        );
+
+        return numbers.Select(int.Parse);
+    }
+
+    private IEnumerable<string> GetDefaultAndExtractedDelimiters(string delimiterLine)
+    {
+        var defaultDelimiters = new List<string> { ",", "\n" };
+
+        if (string.IsNullOrWhiteSpace(delimiterLine) || !delimiterLine.StartsWith("//"))
+        {
+            return defaultDelimiters;
+        }
+
+        var extractedDelimiters = ExtractDelimiters(delimiterLine);
+        var delimiters = defaultDelimiters.Concat(extractedDelimiters);
+
+        return delimiters;
+    }
+
+    private IEnumerable<string> ExtractDelimiters(string delimiterLine)
+    {
+        delimiterLine = delimiterLine.Replace("//", string.Empty);
+        var delimiters = delimiterLine.Split(
+            new[] { "[", "]" },
+            StringSplitOptions.RemoveEmptyEntries
+        );
+
+        return delimiters;
+    }
+}
