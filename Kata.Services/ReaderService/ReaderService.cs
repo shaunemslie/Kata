5cbf844688551c3297821969c3cd6250c9ceafa2
shaunemslie <shaunpemslie@gmail.com>
2023-07-28 20:27:24 +0200

    ReaderService Complete, Defines Entry Method GetParsedNumbersFromInput & ExtractDelimiters, ExterNumbers Methods

---
+++ Kata.Services/ReaderService/ReaderService.cs
@@ -0,0 +1,51 @@
+namespace Kata.Services;
+
+public class ReaderService : IReaderService
+{
+    public IEnumerable<int> GetParsedNumbersFromInput(string input)
+    {
+        var reader = new StringReader(input);
+        var delimiters = new List<string> { ",", "\n" };
+
+        if (input.StartsWith("//"))
+        {
+            var delimiterLine = reader.ReadLine();
+            var extractedDelimiters = GetExtractedDelimiters(delimiterLine!);
+
+            delimiters.AddRange(extractedDelimiters);
+        }
+
+        var inputLessDelimiterLine = reader.ReadToEnd();
+        var numbers = GetExtractedAndParsedNumbers(inputLessDelimiterLine, delimiters);
+
+        return numbers;
+    }
+
+    private IEnumerable<string> GetExtractedDelimiters(string delimiterLine)
+    {
+        var delimiterLineLessPrefix = delimiterLine.Replace("//", string.Empty);
+
+        if (!delimiterLineLessPrefix.Contains('['))
+        {
+            return new[] { delimiterLineLessPrefix };
+        }
+
+        var delimiters = delimiterLineLessPrefix.Split(
+            new[] { '[', ']' },
+            StringSplitOptions.RemoveEmptyEntries
+        );
+
+        return delimiters;
+    }
+
+    private IEnumerable<int> GetExtractedAndParsedNumbers(string input, List<string> delimiters)
+    {
+        var numbers = input.Split(
+            delimiters.ToArray(),
+            StringSplitOptions.RemoveEmptyEntries
+        );
+
+        return numbers.Select(int.Parse);
+    }
+}
+
