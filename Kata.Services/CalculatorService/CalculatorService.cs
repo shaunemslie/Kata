5d3ff763bdc9f25b53692a70c9052419237a111b
shaunemslie <shaunpemslie@gmail.com>
2023-07-28 20:29:42 +0200

    Injects ReaderService, Gets Parsed Numbers & Adds GetValidatedNumbers Method

--- Kata.Services/CalculatorService/CalculatorService.cs
+++ Kata.Services/CalculatorService/CalculatorService.cs
@@ -2,11 +2,29 @@
 
 public class CalculatorService : ICalculatorService
 {
+    private IReaderService _readerService;
+
+    public CalculatorService()
+    {
+        _readerService = new ReaderService();
+    }
+
     public int Add(string numbers)
     {
         if (string.IsNullOrWhiteSpace(numbers))
         {
             return 0;
         }
+
+        var parsedNumbers = _readerService.GetParsedNumbersFromInput(numbers);
+        var validNumbers = GetValidatedNumbers(parsedNumbers);
+        var sum = validNumbers.Sum();
+
+        return sum;
+    }
+
+    private IEnumerable<int> GetValidatedNumbers(IEnumerable<int> numbers)
+    {
+        return numbers.Where(x => x <= 1000);
     }
 }
