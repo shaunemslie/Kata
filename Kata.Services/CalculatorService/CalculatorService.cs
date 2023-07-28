a59128aa65bd1d2e919276c3db48a24211d745af
shaunemslie <shaunpemslie@gmail.com>
2023-07-28 20:12:23 +0200

    Returns 0 When an Empty Input is Passed to Add, Passes First Test

--- Kata.Services/CalculatorService/CalculatorService.cs
+++ Kata.Services/CalculatorService/CalculatorService.cs
@@ -4,6 +4,9 @@ public class CalculatorService : ICalculatorService
 {
     public int Add(string numbers)
     {
+        if (string.IsNullOrWhiteSpace(numbers))
+        {
             return 0;
+        }
     }
 }
